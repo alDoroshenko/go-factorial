b4dffceb1965e282dca0d8d1429c66644c0eb565
aldoroshenko <aldoroshenko@neoflex.ru>
2025-05-19 11:04:01 +0400

    fix comments

--- main.go
+++ main.go
@@ -8,18 +8,17 @@ import (
 func main() {
 	v := in()
 	fmt.Println("Факториал числа ", v, "= ", fact(v))
-	f := factClos()
-	var out int
-	for i := 1; i <= v; i++ {
-		out = f()
-	}
-	fmt.Println("Факториал числа ", v, " через замыкание= ", out)
+	fmt.Println("Факториал числа ", v, " через замыкание = ", callFactClos(v))
+	fmt.Println("Факториал числа ", v, " через рекурсию = ", factRec(v))
 
 }
 func in() int {
 	var val int
 	fmt.Print("Введите число, для вычисления факториала: ")
-	fmt.Scan(&val)
+	_, err := fmt.Scan(&val)
+	if err != nil {
+		log.Fatal("Ошибка при вводе")
+	}
 	if val < 1 {
 		log.Fatal("Введено некорректное значение")
 	}
@@ -40,4 +39,20 @@ func factClos() func() int {
 		res *= count
 		return res
 	}
+}
+
+func callFactClos(v int) int {
+	var out int
+	f := factClos()
+	for i := 1; i <= v; i++ {
+		out = f()
+	}
+	return out
+}
+
+func factRec(v int) int {
+	if v == 0 {
+		return 1
+	}
+	return v * factRec(v-1)
 }
